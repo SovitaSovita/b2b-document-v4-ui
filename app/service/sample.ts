088b34702685d5f53298e345dbce2c53a5293f29
sovita <sovita>
2024-04-26 11:46:10 +0700

    dd

--- app/service/sample.ts
+++ app/service/sample.ts
@@ -2,7 +2,9 @@ import ihttp from "../utils/xhttp";
 
 
 export const sampleFetch = () => {
-    const res = ihttp.post("/admin/welcome", {});
+    const res = ihttp.post("/articles/list", {
+        "dep_id": "1"
+    });
 
     return res;
 }
