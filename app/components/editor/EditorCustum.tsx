b85de54f92c2d281b52c6eca16b7ddd47c3bd87a
MssVanda <Mssda0966120712@gmail.com>
2024-05-07 14:15:19 +0700

    push code

--- app/components/editor/EditorCustum.tsx
+++ app/components/editor/EditorCustum.tsx
@@ -1,7 +1,9 @@
 "use client"
+import { AddArticleBy } from '@/app/service/ArticleService'
+import { GetAllDepartmentId } from '@/app/service/DepartmentService'
 import { DepartmentList } from '@/app/type/DepartmentType'
 import ihttp from '@/app/utils/xhttp'
-import { Editor as TinyMCEEditor } from 'tinymce';
+import { Editor } from '@tinymce/tinymce-react'
 import { hasCustomGetInitialProps } from 'next/dist/build/utils'
 import { useRouter } from 'next/navigation'
 import React, { useEffect,useRef,useState } from 'react'
@@ -11,13 +13,15 @@ import 'semantic-ui-css/semantic.min.css'
 
 
 export default function EditorCustum() {
+
+  const editorRef = useRef(null);
   
   const [addArticleToCart, setArticleToCart] = useState([]);
   const [departments, setDepartments] = useState([]);
   const [selectedDepartment, setSelectedDepartment] = useState<DepartmentList[]>([]);
     const fetchData = async () =>{
       try {
-        const url = await ihttp.get('http://localhost:4545/api/v1/department/AllDepartment');
+        const url = await ihttp.get('http://192.168.178.72:8086/api/v1/department/AllDepartment');
         setSelectedDepartment(url.data.rec);
       } catch (e) {
         //setLoading(false)
@@ -35,7 +39,7 @@ export default function EditorCustum() {
                 alert("Department Can't be empty")
                 return;
             }else{
-                const url = await ihttp.post('http://localhost:4545/api/v1/department/insertDepartment',{ dept_name , created_by })
+                const url = await ihttp.post('http://192.168.178.72:8086/api/v1/department/insertDepartment',{ dept_name , created_by })
                 fetchData();
             }
             
@@ -45,26 +49,15 @@ export default function EditorCustum() {
         }
   }
 
-  const editorRef = useRef<TinyMCEEditor | null>(null);
-  const [content, setContent] = useState(
-    "This is the initial content of the editor."
-  );
 
-  const [text, setText] = useState();
-  const log = () => {
-    if (editorRef.current) {
-      console.log(editorRef.current.getContent());
-    }
-  };
-  
   const router = useRouter();
 
   return (
     <>
       <div className='px-24 mt-14'>
         <form className="ui form">
-          <div className="three fields">
-            <div className="field">
+          <div className="three fields ">
+            <div className="field required">
                 <label>Department</label>
                 <select className="select select-info w-full max-w-xs">
                   {selectedDepartment.map(departments =>(
@@ -74,7 +67,7 @@ export default function EditorCustum() {
                 </select>
                 
             </div>
-            <div className="field">
+            <div className="field required">
               <label>Main Title</label>
               <select className="select select-info w-full max-w-xs">
                 {selectedDepartment.map(departments =>(
@@ -83,46 +76,36 @@ export default function EditorCustum() {
                   }
               </select>
             </div>
-            <div className="field btn " style={{ width: "55px", display: "flex", alignSelf: "center", marginBottom: "-22px" }}>
-              <button className="btn btn-active" id='addArticle'>
-                Add
-              </button>
-            </div>
             <div className="field required">
               <label>Sub Title</label>
               <input type="text" placeholder="Type here" className="input input-bordered w-full max-w-xs" />
             </div>
           </div>
           <Editor
-
             apiKey='ibgazhdpbf1641m9l0exn7y2y0pbcwbtlmz013z4uf1icb2e'
-            onEditorChange={onEditorChange}
-
-            value={content}
-            //onInit={(evt, editor) => editorRef.current = editor}
-            //initialValue="<p>This is the initial content of the editor.</p>"
+           // onInit={(_evt, editor) => editorRef.current = editor}
+            initialValue="<p>This is the initial content of the editor.</p>"
             init={{
               height: 500,
-              menubar: false,
+              menu:{
+                insert: { title: 'Insert', items: 'insertfile' },
+              } ,
+              file_picker_types: 'image',
+              insert_button_items: 'insertfile',
+              images_upload_credentials: true,
               plugins: [
-                "mentions advlist autolink lists link image charmap print preview anchor",
-                "searchreplace visualblocks code fullscreen",
-                "insertdatetime media paste code help wordcount",
+                'advlist', 'autolink', 'lists','list link image table wordcount', 'link', 'charmap', 'preview','file insert','image',
+                'anchor', 'searchreplace', 'visualblocks', 'code', 'fullscreen',
+                'insertdatetime', 'media', 'table', 'code', 'help', 'wordcount','tinydrive'
               ],
-              toolbar:
-                "undo redo | formatselect | " +
-                "bold italic backcolor | alignleft aligncenter " +
-                "alignright alignjustify | bullist numlist outdent indent | " +
-                "removeformat | emoticons| help",
-              content_style:
-                "body { font-family:Helvetica,Arial,sans-serif; font-size:14px }",
-              emoticons_append: {
-                custom_mind_explode: {
-                  keywords: ["brain", "mind", "explode", "blown"],
-                  char: "🤯",
-                },
-              },
+              toolbar: 'undo redo | blocks | ' + 'list link image table wordcount'+'image'+'file insert'+'insert'+
+                'bold italic forecolor | alignleft aligncenter ' +
+                'alignright alignjustify | bullist numlist outdent indent | ' +
+                'removeformat | help',
+              images_file_types: 'jpg,svg,webp',
+              content_style: 'body { font-family:Helvetica,Arial,sans-serif; font-size:14px }'
             }}
+            
           />
         </form>
         <div className='mt-14 flex justify-end'>
@@ -132,5 +115,5 @@ export default function EditorCustum() {
       </div>
     </>
   )
-}
+
 }
