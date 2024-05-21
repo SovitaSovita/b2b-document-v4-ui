9eb3eaaa0ec23135c446fa9da72a119dbfd64e3f
Laychanvimean <lovebin301@gamil.com>
2024-05-21 14:30:52 +0700

    update

--- app/components/Material/Loading.tsx
+++ app/components/Material/Loading.tsx
@@ -2,7 +2,7 @@
 export default function LoadingCustom() {
     return (
         <div className="flex justify-center items-center min-h-screen">
-            <span className="loading loading-spinner loading-lg"></span>
+            <span className="loading loading-spinner loading-lg"></span> 
         </div>
     )
 }
