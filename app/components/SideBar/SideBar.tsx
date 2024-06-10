9bf37b4f8a91850b17f5b050896f6e2986450064
Laychanvimean <lovebin301@gamil.com>
2024-06-10 14:56:12 +0700

    updaye

--- app/components/SideBar/SideBar.tsx
+++ app/components/SideBar/SideBar.tsx
@@ -22,6 +22,7 @@ import HeaderSidebar from './HeaderSidebar';
 import { RootState } from '@/app/service/Redux/store/store';
 import { getSession } from '@/app/utils/xhttp';
 import LoadingCustom from '../Material/Loading';
+import { Tooltip } from '@nextui-org/react';
 
 const drawerWidth = 320;
 
@@ -164,14 +165,16 @@ function SideBar(props: any) {
                                             <li key={index + 1} className='w-[260px]'>
                                                 <details>
                                                     <summary className="mt-1 font-medium">
-                                                        <p data-tip="This is a tooltip" data-for="tooltipId" className='line-clamp-1'>{item.title}</p>
+                                                        <Tooltip showArrow={true} content={item.title}>
+                                                            <p data-tip="This is a tooltip" data-for="tooltipId" className='line-clamp-1'>{item.title}</p>
+                                                        </Tooltip>
                                                     </summary>
                                                     <ul>
                                                         {
                                                             filterArticlesByTagId(item.id).length > 0 ?
                                                                 filterArticlesByTagId(item.id).map(item => (
                                                                     <li key={item?.id} onClick={() => handleViewArticle(item.id.toString())}>
-                                                                        <a  className={activeItemId === item.id.toString()
+                                                                        <a className={activeItemId === item.id.toString()
                                                                             ? "hover:bg-base-100 line-clamp-1 bg-base-100 border-r-4 border-secondary rounded-none"
                                                                             : "hover:bg-base-100 line-clamp-1 hover:border-l-4 border-secondary rounded-none transition-all"}>
                                                                             {item?.title}
