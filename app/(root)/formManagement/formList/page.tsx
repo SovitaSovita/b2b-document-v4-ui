94ebac240f929312d9c31e697786901695a6efc8
@rayuthek430 <rayuthek430@gmail.com>
2024-06-24 17:27:46 +0700

    pull

--- app/(root)/formManagement/formList/page.tsx
+++ app/(root)/formManagement/formList/page.tsx
@@ -5,7 +5,8 @@ import React, { useEffect, useState } from 'react'
 import { Button, Card, CardBody, CardHeader, Image } from '@nextui-org/react'
 import { ListAllFormName } from '@/app/service/FormManagement'
 import { useParams } from 'next/navigation'
-import { useDispatch } from 'react-redux'
+import { useDispatch, useSelector } from 'react-redux'
+import { RootState } from '@/app/service/Redux/store/store'
 
 interface Form {
     id: number,
@@ -17,13 +18,19 @@ interface Form {
 }
 
 function Page() {
+    const session: UserData = useSelector((state: RootState) => state?.article.session);
+    console.log("session :) ", session.userId)
+
     const [forms, setForms] = useState<Form[]>([]);
 
 
     useEffect(() => {
         const fetchAPI = async () => {
             try {
-                const response = await ListAllFormName(2, "sovita") as { data?: { rec: Form[] } };
+                const response = await ListAllFormName({
+                    "userId": "vimean",
+                    "status": 0
+                }) as { data?: { rec: Form[] } };
                 if (response && response.data && response.data.rec) {
                     setForms(response.data.rec);
                 } else {
@@ -42,9 +49,9 @@ function Page() {
             <div className='p-4'>
                 <div className="gap-2 grid grid-cols-2 sm:grid-cols-4">
                     {forms.map((form) => (
-                        <Card key={form.id} className="py-4">
+                        <Card key={form.id} className="py-4 shadow-none border">
                             <CardHeader className="pb-0 pt-2 px-4 flex-col items-start">
-                                <p className="text-tiny uppercase font-bold">Form Name</p>
+                                {/* <p className="text-tiny uppercase font-bold">Form Name</p> */}
                                 <h4 className="font-bold text-large">{form.formName}</h4>
                             </CardHeader>
                             <CardBody className="overflow-visible py-2">
@@ -53,6 +60,17 @@ function Page() {
                         </Card>
                     ))}
                 </div>
+            </div>
+            <div className='flex flex-col w-full h-full'>
+                <div className='mb-4 flex items-center justify-between'>
+                    {/* Left side icons */}
+                    <div className='p-2'>
+
+                    </div>
+                    {/* Right side icons */}
+
+                </div>
+                    <p>aHello</p>
             </div>
         </>
     )
